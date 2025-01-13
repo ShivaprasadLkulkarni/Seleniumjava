7aa335428250253079e94a7e040534b32558d395
SHivaprasad L Kulkarni <Shivaprasadlkulkarni@gmail.com>
2025-01-13 15:51:11 +0530

    firdt commit

---
+++ src/main/java/JavaProject/Project/variableDemo.java
@@ -0,0 +1,19 @@
+package JavaProject.Project;
+
+public class variableDemo {
+	// Global variable is intialized or declared here
+	
+	static int my_number=8174342; // have not initialized any value still not giving error coz by default it assigned null 
+	
+public static void main(String[] args) {
+//	 Facts
+//	 Local variable and garbage variable
+//  Global variable contains null value/default values
+ //	 Variable Demo
+	 
+    //	 local variable
+	 int a=0;
+	 System.out.println(a);//untill initilazing it says not intilaizaed
+	 System.out.println(my_number);
+}
+}
