260f33a5c9107db9ae3a7ab775c7fb3bebaf9d21
SHivaprasad L Kulkarni <Shivaprasadlkulkarni@gmail.com>
2025-01-15 16:34:46 +0530

    Files organized alphabetically and 2 new files added loop and conditional

---
+++ src/main/java/JavaProject/Project/Conditionalsdemod.java
@@ -0,0 +1,164 @@
+package JavaProject.Project;
+
+public class Conditionalsdemod {
+public static void main(String[] args) {
+	 
+//	if condition
+//	syntax
+//	if(condition) {
+//		logic
+//	}
+//	System.out.println("Test Started");
+//	int a= 91;
+//	
+//	if(a<=100) {
+//		System.out.println("Value of a is this : "+a);
+//	}
+//	
+//	if(a<=50) {
+//		System.out.println("Value of a is this : "+a);
+//	}
+//	
+//		System.out.println("Test end");
+	
+//	if else condition 
+//	syntax
+//	if() {
+//		
+//	}
+//	else {
+//		
+//	}
+//	int a= 1001111;
+//	
+//if(a<1111) 	{
+//		
+//		System.out.println("res a : " +a);
+//		
+//	}
+//	
+//if(a<1111) 	{
+//		
+//		System.out.println("res b: " +a);
+//		
+//	}
+//else {
+//	
+//		System.out.println("Not Valisd" );
+//	}
+	
+//System.out.println("Test started");
+//String browser = "Chrome";
+//
+//if(browser.equalsIgnoreCase("Firefox")) {  // if u dont have integer dtattype and if u have string type then must use an equalIgnorCase
+//	System.out.println("TC executed on firefox");
+//}
+//else
+//{
+//System.out.println("TC executed on Chrome");
+//}
+//
+//System.out.println("Test Ended");	
+
+//	Nested if else //  Used in Selenium
+//	syntax
+//	if() {
+//		
+//	}elseif(){
+//		
+//	}elseif(){
+//		
+//	}else {
+//		
+//	}
+	
+//	System.out.println("Test started");
+//	String browser = "IE";
+//
+//	if(browser.equalsIgnoreCase("Chrome")) {  
+//    System.out.println("TC executed on Chrome");
+//	}
+//	else if(browser.equalsIgnoreCase("FireFox"))
+//	{
+//	System.out.println("TC executed on Firefox");
+//	}
+//	else if(browser.equalsIgnoreCase("IE"))
+//	{
+//	System.out.println("TC executed on IE");
+//	}
+//	else {
+//		System.out.println("Kindly provide valid name");
+//	}
+//	System.out.println("Test Ended");
+	
+//	Switch Statement 
+//	not used multiple time but it is an important
+//	syntax
+	
+//	switch(expression)
+//	{
+//	case value;	
+//	//code
+//    break;
+//	case value;
+//	//code
+//	break;
+//	//any numb of case statement
+//	
+//	default:
+//	}
+	
+     int x=5;
+     
+     switch(x)
+     {
+     
+     case 1:
+    	 System.out.println("poor");
+    	 break;
+    	 
+     case 2:
+    	 System.out.println("Slow");
+    	 break;
+    	 
+     case 3:
+    	 System.out.println("Good");
+    	 break;
+    	 
+     case 4:
+    	 System.out.println("Nice");
+    	 break;
+    	 
+     case 5:
+    	 System.out.println("Excellent");
+    	 break;
+    	 
+     default:
+    	 System.out.println("Reerite");
+     }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
---
+++ src/main/java/JavaProject/Project/loopdemoc.java
@@ -0,0 +1,47 @@
+package JavaProject.Project;
+
+public class loopdemoc {
+
+	public static void main(String[] args) {
+		
+//      For loop
+//      basic loop -widely use
+//      Syntax
+		
+//		for(initialize;condition;increment/decrement)
+//		{
+//		Code herby/logic here 
+//		}
+		
+//		System.out.println("loop started ");
+//      for(int i=1;i<=20;i=i+2)// if u give <= it will print 20 also
+//      {
+//		System.out.println("I values are : "+i);
+//	     }
+//      System.out.println("loop ended");
+
+
+//     While loop
+		
+//	   While(condition) {	
+//	   logic here
+//	   increment or decrement
+//	   }
+
+//		int count =10;
+//		while(count<=80) {
+//		System.out.println("values are : "+count);
+//		count++;
+//		}
+		
+		//Do While
+		//Do while loop not use in selenium but for knowledge
+//		logic is executed atleast once in a loop
+		
+//		int ticket =90;
+//		do {
+//		System.out.println("current ticket number is " +ticket );
+//		ticket++;
+//		}while(ticket<=100);
+
+	}}
